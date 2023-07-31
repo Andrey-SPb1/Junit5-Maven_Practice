6706602b2dfcb22082e35e1446a652be598c8954
Andrey <honor8x98@gmail.com>
2023-07-31 20:20:09 +0300

    Initial commit

---
+++ src/test/java/com/junit/service/UserServiceTest.java
@@ -0,0 +1,13 @@
+package com.junit.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class UserServiceTest {
+
+    @Test
+    void test() {
+        fail();
+    }
+}
