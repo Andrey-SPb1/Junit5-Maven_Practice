e6ba8b4893b973412ca03a6a7f205d653144d584
Andrey <honor8x98@gmail.com>
2023-08-06 14:00:22 +0300

    Launcher API

---
+++ src/test/java/com/junit/TestLauncher.java
@@ -0,0 +1,32 @@
+package com.junit;
+
+import com.junit.service.UserServiceTest;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+
+import java.io.PrintWriter;
+
+public class TestLauncher {
+
+    public static void main(String[] args) {
+        var launcher = LauncherFactory.create();
+        var summaryGeneratingListener = new SummaryGeneratingListener();
+
+
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder
+                .request()
+                .selectors(DiscoverySelectors.selectPackage("com.junit.service"))
+//                .selectors(DiscoverySelectors.selectClass(UserServiceTest.class))
+                .build();
+
+        launcher.execute(request, summaryGeneratingListener);
+
+        try(var writer = new PrintWriter(System.out)) {
+            summaryGeneratingListener.getSummary().printTo(writer);
+        }
+    }
+}
--- src/test/java/com/junit/service/UserServiceTest.java
+++ src/test/java/com/junit/service/UserServiceTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class UserServiceTest {
+public class UserServiceTest {
 
     private UserService userService;
 
