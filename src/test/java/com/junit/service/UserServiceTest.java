492370eaf4ce0ea801eeb726034bb593a8935fb0
Andrey <honor8x98@gmail.com>
2023-08-07 18:29:58 +0300

    Launcher API

--- src/test/java/com/junit/service/UserServiceTest.java
+++ src/test/java/com/junit/service/UserServiceTest.java
@@ -3,10 +3,15 @@ package com.junit.service;
 import com.junit.entity.User;
 import org.junit.jupiter.api.*;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceTest {
+
+    private static final User IVAN = User.of(1, "Ivan", "213");
+    private static final User PETR = User.of(2, "Petr", "421");
 
     private UserService userService;
 
@@ -29,10 +34,32 @@ public class UserServiceTest {
     }
 
     @Test
+    void loginSuccessIfUserExists() {
+        userService.add(IVAN);
+        Optional<User> maybeUser = userService.login(IVAN.getUsername(), IVAN.getPassword());
+        assertTrue(maybeUser.isPresent());
+        maybeUser.ifPresent(user -> assertEquals(IVAN, user));
+    }
+
+    @Test
+    void loginFailIfPasswordIsNotCorrect() {
+        userService.add(IVAN);
+        Optional<User> maybeUser = userService.login(IVAN.getUsername(), "qwerty");
+        assertTrue(maybeUser.isEmpty());
+    }
+
+    @Test
+    void loginFailIfUserDoesNotExist() {
+        userService.add(IVAN);
+        Optional<User> maybeUser = userService.login("none", IVAN.getPassword());
+        assertTrue(maybeUser.isEmpty());
+    }
+
+    @Test
     void usersSizeIfUsersAdded() {
         System.out.println("Test 2: " + this);
-        userService.add(new User());
-        userService.add(new User());
+        userService.add(IVAN);
+        userService.add(PETR);
 
         var users = userService.getAll();
         assertEquals(2, users.size());
